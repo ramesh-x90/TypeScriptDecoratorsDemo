9f43db115f04844e0bac94847bc157b9e7115098
Ramesh Shyaman <rameshshyaman@gmail.com>
2022-12-29 11:18:33 +0530

    comment

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,19 @@ import "reflect-metadata";
 const textMetaData = "text";
 
 
+// method decorator
 function first(mod: string = "hello") {
 
     return function (target: object, key: string, descriptor: PropertyDescriptor) {
 
         const original: Function = descriptor.value;
 
+        // read metadata
         let existingMetaData: number[] = Reflect.getMetadata(textMetaData, target, key)
+
         console.log("param type: " + Reflect.getMetadata("design:paramtypes", target, key)[0].name)
 
+        // wrapper for original method
         descriptor.value = function (...args: any[]) {
 
             for (const i of existingMetaData) {
@@ -30,12 +34,14 @@ function first(mod: string = "hello") {
 }
 
 
-
+// param decorator
 function text() {
     return function (target: Object, key: string | symbol, parameterIndex: number) {
 
+        // get metadata
         let existingMetaData: number[] = Reflect.getMetadata(textMetaData, target, key) || []
         existingMetaData.push(parameterIndex)
+        // add metadata
         Reflect.defineMetadata(textMetaData, existingMetaData, target, key)
     }
 }
